913389667bbf6e3d938f7e356d083ae502a3dfff
ariqzuhdi <ariqzhd@gmail.com>
2026-03-15 22:43:10 +0700

    update db

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,12 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 	dsn := os.Getenv("DB_URL")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true, // Disables pgx driver prepared statements
+	}), &gorm.Config{
+		PrepareStmt: false, // Disables GORM prepared statements
+	})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database")
